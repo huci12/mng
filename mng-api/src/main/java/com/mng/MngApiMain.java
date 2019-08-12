4233874ea44f3e93152f85763d3ab2d4c3c368db
burns.choi <huci12@naver.com>
2019-08-12 23:54:54 +0900

    SpringBootApplication 기본 생성

--- mng-api/src/main/java/com/mng/MngApiMain.java
+++
@@ -1,5 +0,0 @@
-package com.mng;
-
-public class MngApiMain {
-
-}
---
+++ mng-api/src/main/java/com/mng/api/MngApiApplication.java
@@ -0,0 +1,11 @@
+package com.mng.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngApiApplication {
+	public static void main(String []args) {
+		SpringApplication.run(MngApiApplication.class, args);
+	}
+}
